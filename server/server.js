dc7df7cbf71adc6a77469d2206234a736754eae0
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-10-01 18:17:03 +0300

    (fix) remove Sequelize orm from server file

--- server/server.js
+++ server/server.js
@@ -1,47 +1,90 @@
 var express = require('express');
-//var mongoose = require('mongoose');
+var Sequelize = require("sequelize");
 var app = express();
 var server = require ('http').createServer(app);
 
 
-//============================================================
-//var mongoURI = process.env.MONGODB_URI ||'DataBase Name';
-//mongoose.connect(mongoURI);
-//db = mongoose.connection;
-// db.once('open',function () {
-// 	console.log('mongoDB is open');
-// });
-//============================================================
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database: 'rbkadmission'
-});
-
-connection.connect();
-
-connection.query('SELECT * from candidateinfo', function(err, rows, fields) {
-  if (err){
-  	throw err;
-  }else{
-  	console.log('The solution is: ' , rows.count);
-  }
-});
-
-connection.end();
-
-
-
 var port = process.env.PORT || 3020;
-
 require('./config/middleware.js') (app,express);
 require('./config/routes.js') (app,express);
-
 app.listen(port , function () {
 	console.log('Server now listening on port ' + port);
 });
 
+
+
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+		// mysql database connection
+//============================================================
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'root',
+//   database: 'rbkadmission'
+// });
+
+// connection.connect();
+
+// connection.query('SELECT * from candidateinfo', function(err, rows, fields) {
+//   if (err){
+//   	throw err;
+//   }else{
+//   	console.log('The solution is: ' , rows.count);
+//   }
+// });
+
+// connection.end();
+//============================================================
+
+*/
