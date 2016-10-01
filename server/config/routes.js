7506f9f5a4e38985b92c506349c33cc9c7aebb06
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-10-01 18:16:11 +0300

    (fix) api/links & req lib

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,9 @@
+var Video = require('../database/databaseController.js');
 var helpers = require('./helpers.js'); // our custom middleware
 
 module.exports = function (app, express) {
-  // authentication middleware used to decode token and made available on the request
-    //  app.get('/api/links/', linksController.allLinks);
-    //app.post('/api/links/', linksController.newLink);
 
+	
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
   app.use(helpers.errorLogger);
