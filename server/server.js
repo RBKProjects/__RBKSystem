75e18d0cf5abe2f0d3f00b7917ec40e8e3e8af84
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-10-02 09:17:36 +0300

    (fix) rewrite server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ var Sequelize = require("sequelize");
 var app = express();
 var server = require ('http').createServer(app);
 
-
 var port = process.env.PORT || 3020;
 require('./config/middleware.js') (app,express);
 require('./config/routes.js') (app,express);
@@ -11,80 +10,4 @@ app.listen(port , function () {
 	console.log('Server now listening on port ' + port);
 });
 
-
-
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-		// mysql database connection
-//============================================================
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database: 'rbkadmission'
-// });
-
-// connection.connect();
-
-// connection.query('SELECT * from candidateinfo', function(err, rows, fields) {
-//   if (err){
-//   	throw err;
-//   }else{
-//   	console.log('The solution is: ' , rows.count);
-//   }
-// });
-
-// connection.end();
-//============================================================
-
-*/
