d728cff47cb97d85621e5f86b7a1959fcf13d63c
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-10-03 12:02:14 +0300

    (feat) add route for the staff

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,26 +1,32 @@
 var candidateinfo = require('../database/candidateinfoController.js');
-var helpers = require('./helpers.js'); // our custom middleware
+var staff = require('../database/staffController.js');
+var helpers = require('./helpers.js'); 
 
 module.exports = function (app, express) {
-
 
 	//============================================================================
 	/* 								candidateinfo  								*/
 	//============================================================================
-	app.post('/api/candidateinfo/updateCandidate', candidateinfo.updateCandidate);
-	// app.post('/api/candidateinfo/signin',candidateinfo.signin);
-	// app.post('/api/candidateinfo/signup',candidateinfo.signup);
-	// app.get('/api/candidateinfo/getAllCandidate', candidateinfo.getAllCandidate);
-	// app.get('/api/candidateinfo/getCandidate', candidateinfo.getCandidate);
-	// app.get('/api/candidateinfo/', candidateinfo.getUsers);
-	// app.get('/api/candidateinfo/', candidateinfo.getUsers);
-	
+		app.post('/api/candidateinfo/updateCandidate', candidateinfo.updateCandidate);
+		app.post('/api/candidateinfo/signin',candidateinfo.signin);
+		app.post('/api/candidateinfo/signup',candidateinfo.signup);
+		app.get('/api/candidateinfo/getAllCandidate', candidateinfo.getAllCandidate);
+		app.get('/api/candidateinfo/getCandidate', candidateinfo.getCandidate);
+		
+	//============================================================================
+	/* 									staff   								*/
+	//============================================================================
+		app.post('/api/staff/signin',staff.signin);
+		app.post('/api/staff/signup',staff.signup);
+		app.post('/api/staff/updateCandidate',staff.updateCandidate);
+		app.post('/api/staff/updateInfo',staff.updateInfo);
 
-
-	
-  // If a request is sent somewhere other than the routes above,
-  // send it through our custom error handler
-  app.use(helpers.errorLogger);
-  app.use(helpers.errorHandler);
+	//============================================================================
+	/* 								erorr case  								*/
+	//============================================================================	
+	  	// If a request is sent somewhere other than the routes above,
+	  	// send it through our custom error handler
+	  	app.use(helpers.errorLogger);
+	  	app.use(helpers.errorHandler);
 };
 
