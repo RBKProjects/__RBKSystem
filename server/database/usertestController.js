4697bfb4497cdd1d2f450cee6d09eb6dfb749e8b
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-10-05 14:13:29 +0300

    (feat) usertestController function

--- server/database/usertestController.js
+++ server/database/usertestController.js
@@ -5,9 +5,37 @@ var jwt = require('jwt-simple');
 //declare connection 
 var connection = db ;
 
+var insertToUsertest = function (userId, insertedData, key, res) {
+	var updatePost = {};
+	var insertPost = {};
+	updatePost[key] = insertedData;
+	insertPost[key] = insertedData;
+	insertPost.userinfo_id = userId;
+	
+	var sql    = 'SELECT * FROM usertest WHERE userinfo_id = ' + connection.escape(userId);
+	connection.query(sql, function(err, rows, fields) {
+	  if (!err){
+	  		if(rows.length > 0){
+	  			connection.query('UPDATE usertest SET ? WHERE id = ?', [updatePost, userId]); 
+		  		res.json({rows : rows});
+		  	}else{
+				connection.query('INSERT INTO usertest SET ?', insertPost, function (err, rows) {
+					if (err){
+						throw err;
+					} else{
+						res.json({rows : rows});
+					}
+				})
+		    }
+	  }else{
+	  	throw err
+	  };
+	});
+}
+
 module.exports = {
 
-	getALLTest : function (req, res) {
+	getALLUsersTests : function (req, res) {
 		var sql = 'SELECT * FROM usertest';
 		connection.query(sql, function(err, rows, fields) {
 		  if (!err){
@@ -19,7 +47,7 @@ module.exports = {
 		});
 	},
 
-	getTest : function (req, res) {
+	getUserTests : function (req, res) {
 		var sql    = 'SELECT * FROM usertest WHERE id = ' + connection.escape(req.body.id);
 		connection.query(sql, function(err, rows, fields) {
 		  if (err) throw err;
@@ -29,32 +57,24 @@ module.exports = {
 	},
 
 	insertPersonality : function (req, res) {
-		var candidate = req.body;
-		var newCandidate = {
-			userinfo_id : candidate.userinfo_id,
-			agreementqustions_id : candidate.agreementqustions_id,
-			answer : candidate.answer
-		}
-		var sql    = 'SELECT * FROM agreementanswer WHERE userinfo_id = ' + connection.escape(candidate.userinfo_id) + 'AND agreementqustions_id =' + connection.escape(candidate.userinfo_id);
-		connection.query(sql, function(err, rows, fields) {
-		  if (!err){
-		  		if(rows.length > 0){ 
-			  		res.json({rows : rows, msg : 'User exist'});
-			  	}else{
-					connection.query('INSERT INTO agreementanswer SET ?', newCandidate, function (err, rows) {
-						if (err){
-							throw err;
-						} else{
-							res.json({rows : rows});
-					    	console.log(rows.insertId);
-						}
-					})
-			    }
-		  }else{
-		  	throw err
-		  };
+		insertToUsertest(req.body.id, req.body.personality, "personality", res);
+	},
 
-		});
-	} 
+	insertAnalitical : function (req, res) {
+		insertToUsertest(req.body.id, req.body.analitical, "analitical", res);	
+	},
+
+	insertCodeExp : function (req, res) {
+		insertToUsertest(req.body.id, req.body.codeExp, "codeExp", res);	
+	},
+
+	insertCodeSyntax : function (req, res) {
+		insertToUsertest(req.body.id, req.body.codeSyntax, "codeSyntax", res);	
+	},
+
+	insertMindSet : function (req, res) {
+		insertToUsertest(req.body.id, req.body.mindSet, "mindSet", res);	
+	}
+
 }
  
