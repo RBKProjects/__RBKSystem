698a08a51b14e2b29ebdec3929fe805e4fea442a
Fatima90 <en.f.h90@gmail.com>
2016-10-20 17:20:02 +0300

    fixing agreement questions

--- client/app/views/rbkAgreement/rbkAgreement.js
+++ client/app/views/rbkAgreement/rbkAgreement.js
@@ -3,7 +3,7 @@ angular.module('Admission.agreement',[])
 	$scope.agreement={};
 	$scope.questions={};
 	$scope.questionOptions={};
-	$scope.q_id="";
+	$scope.questions=[];
 
 	$scope.sendAgreement=function(){
 		var userId=$window.localStorage.getItem('user');
@@ -14,26 +14,54 @@ angular.module('Admission.agreement',[])
 		// })
 	};
 
+	$scope.getQuestion=function(){
+		Agreement.getQuestion()
+		.then(function(resp){
+
+		})
+	}
 	$scope.getQuestions=function(){
 		Agreement.getAllQuestions()
 		.then(function(resp){
-		//	console.log(resp.rows);
-			$scope.questions=resp.rows;
+			 //save all questions id and questions in an array
+			 for (var i = 0; i < resp.rows.length; i++) {
+			 	$scope.questions.push({});
+			 	$scope.questions[i][resp.rows[i]['id']]=[];
+			 	$scope.questions[i][resp.rows[i]['id']].push(resp.rows[i]['id']);
+			 	$scope.questions[i][resp.rows[i]['id']].push(resp.rows[i]['arabicQ']);
+			 	$scope.questions[i][resp.rows[i]['id']].push(resp.rows[i]['englishQ']);
+			 }
+
 			_.map(resp.rows,function(row){
-				console.log('row np',row['id'])
-				$scope.q_id=row['id'];
 				$scope.getQuestionOptions(row['id']);
+				console.log(row['id'])
 			})
 		})
 	};
 
-	$scope.getQuestionOptions=function(id){
-		Agreement.getQuestionOptions(id)
+	$scope.getQuestionOptions=function(id,question){
+		var obj={q_id : id};
+		Agreement.getQuestionOptions(obj)
 		.then(function(resp){
-			console.log(resp.data);
-			//$scope.questionOptions=resp;
+			_.map($scope.questions,function(row){
+				//if(row['Q_id'] === id){
+				console.log($scope.questions[row])
+
+				//$scope.questions[row][id].push(row['text_a']);
+				//$scope.questions[id].push(row['text_e']);
+				//}
+			})
+			// console.log(resp.data.rows[0]);
+			// console.log(resp.data.rows[1]['Q_id']);
+			// console.log($scope.questions[0][resp.data.rows[0]['Q_id']]);
+			//$scope.questions[resp.data.rows[0]['Q_id']].push(resp.data.rows[0]);
+			//$scope.questions[resp.data.rows[1]['Q_id']].push(resp.data.rows[0]);
+			//console.log($scope.questions);
+			//$scope.questionOptions[id]=[resp.data.rows,question];
+			//$scope.questionOptions={id:resp.data.rows};
+			//console.log($scope.questionOptions)
 		});
 	};
-
 	$scope.getQuestions();
+	//$scope.getQuestions();
 })
