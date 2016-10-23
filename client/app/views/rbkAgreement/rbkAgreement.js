7cef4b6c0a9fe87fcf51a3328e529cdf9525edef
Fatima90 <en.f.h90@gmail.com>
2016-10-23 18:02:50 +0300

    agreement questions

--- client/app/views/rbkAgreement/rbkAgreement.js
+++ client/app/views/rbkAgreement/rbkAgreement.js
@@ -1,16 +1,21 @@
 angular.module('Admission.agreement',[])
-.controller('AgreenmentController',function($scope,$window,Agreement){
+.controller('AgreenmentController',function($scope,$window,Agreement,$location){
 	$scope.agreement={};
-	$scope.questions={};
+	//$scope.questions={};
 	$scope.questionOptions={};
 	$scope.questions=[];
+	$scope.options={};
+	$scope.agreementAnswers={};
 
 	$scope.sendAgreement=function(){
 		var userId=$window.localStorage.getItem('user');
 		console.log(userId, $scope.agreement)
+		._map($scope.agreement,function(row){
+			
+		})
 		// Agreement.sendAnswers(userId)
 		// .then(function(){
-		// 	$location.path('/videoIntro')
+			//$location.path('/videoIntro')
 		// })
 	};
 
@@ -24,18 +29,14 @@ angular.module('Admission.agreement',[])
 		Agreement.getAllQuestions()
 		.then(function(resp){
 			 //save all questions id and questions in an array
-			 for (var i = 0; i < resp.rows.length; i++) {
+			for (var i = 0; i < resp.rows.length; i++) {
 			 	$scope.questions.push({});
-			 	$scope.questions[i][resp.rows[i]['id']]=[];
-			 	$scope.questions[i][resp.rows[i]['id']].push(resp.rows[i]['id']);
-			 	$scope.questions[i][resp.rows[i]['id']].push(resp.rows[i]['arabicQ']);
-			 	$scope.questions[i][resp.rows[i]['id']].push(resp.rows[i]['englishQ']);
+			 	$scope.questions[resp.rows[i]['id']]=[];
+			 	$scope.questions[resp.rows[i]['id']].push(resp.rows[i]['id']);
+			 	$scope.questions[resp.rows[i]['id']].push(resp.rows[i]['englishQ']);
+			 	$scope.questions[resp.rows[i]['id']].push(resp.rows[i]['arabicQ']);
+				$scope.getQuestionOptions(resp.rows[i]['id']);
 			 }
-
-			_.map(resp.rows,function(row){
-				$scope.getQuestionOptions(row['id']);
-				console.log(row['id'])
-			})
 		})
 	};
 
@@ -43,25 +44,20 @@ angular.module('Admission.agreement',[])
 		var obj={q_id : id};
 		Agreement.getQuestionOptions(obj)
 		.then(function(resp){
-			_.map($scope.questions,function(row){
-				//if(row['Q_id'] === id){
-				console.log($scope.questions[row])
-
-				//$scope.questions[row][id].push(row['text_a']);
-				//$scope.questions[id].push(row['text_e']);
-				//}
+			if(!$scope.options[id]){
+				$scope.options[id]=[];
+			}
+			_.map(resp.data.rows,function(row){
+					if(row['Q_id'] === id){
+						var innerAr=[];
+						innerAr.push(row['id']);
+						innerAr.push(row['text_a']);
+						innerAr.push(row['text_e']);
+						$scope.options[id].push(innerAr);
+					}
 			})
-			// console.log(resp.data.rows[0]);
-			// console.log(resp.data.rows[1]['Q_id']);
-			// console.log($scope.questions[0][resp.data.rows[0]['Q_id']]);
-			//$scope.questions[resp.data.rows[0]['Q_id']].push(resp.data.rows[0]);
-			//$scope.questions[resp.data.rows[1]['Q_id']].push(resp.data.rows[0]);
-			//console.log($scope.questions);
-			//$scope.questionOptions[id]=[resp.data.rows,question];
-			//$scope.questionOptions={id:resp.data.rows};
-			//console.log($scope.questionOptions)
 		});
 	};
+	
 	$scope.getQuestions();
-	//$scope.getQuestions();
 })
